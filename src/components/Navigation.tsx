9ed92b1e1ee5a85e4b70d4c643350d4b19d2171a
Bogdan <bsshul@gmail.com>
2022-12-05 14:47:06 +0200

    Main screen finished

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,24 +1,24 @@
 const Burger = () => {
   return (
-    <div className="w-fit h-fit flex items-center justify-center cursor-pointer">
-      <span className="block bg-myDark h-[2px] w-[30px] absolute top-0"></span>
-      <span className="block bg-myDark h-[2px] w-[30px] absolute top-2.5"></span>
+    <div className="w-[2rem] h-[2rem] flex flex-col gap-2 items-center justify-center cursor-pointer">
+      <span className="bg-myDark h-[2px] w-[2rem]"></span>
+      <span className="bg-myDark h-[2px] w-[2rem]"></span>
     </div>
   );
 };
 
 const Plus = () => {
   return (
-    <div className="w-fit flex items-center justify-center cursor-pointer">
-      <span className="block bg-myDark h-[2px] w-[25px] absolute top-0"></span>
-      <span className="block bg-myDark h-[2px] w-[25px] rotate-90 absolute top-0"></span>
+    <div className="w-[1.8rem] h-[1.8rem] flex items-center justify-center cursor-pointer">
+      <span className="bg-myDark h-[2px] w-[1.8rem]"></span>
+      <span className="bg-myDark h-[2px] w-[1.8rem] rotate-90 absolute"></span>
     </div>
   );
 };
 
 export const Navigation = () => {
   return (
-    <div className="px-8 my-5 flex justify-between relative">
+    <div className="px-4 mt-4 mb-2 flex justify-between relative">
       <Burger />
       <Plus />
     </div>
