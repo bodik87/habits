b268fa8b15c7f6687cb22f8772b905e7273f1162
Bogdan <bsshul@gmail.com>
2022-12-08 11:42:42 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,32 +5,34 @@ interface DateGroupProps {
   date: string;
 }
 
-let daysInterval = eachDayOfInterval({
-  start: subDays(startOfToday(), 4),
-  end: startOfToday(),
-});
-
-let datesRow: any = [];
-daysInterval.reverse().forEach((day) => {
-  datesRow.push(format(day, "MM-ee").toString());
-});
-
 const DateGroup = ({ day, date }: DateGroupProps) => {
   return (
     <div className="flex flex-col items-center">
-      <div className="font-medium leading-none">{date}</div>
-      <div className="text-[0.8rem] font-medium leading-none">{day}</div>
+      <div className="font-medium leading-none">{date.slice(0, 2)}</div>
+      <div className="text-[0.8rem] font-medium leading-none">
+        {day.slice(-3)}
+      </div>
     </div>
   );
 };
 
 export const Header = () => {
+  let daysInterval = eachDayOfInterval({
+    start: subDays(startOfToday(), 4),
+    end: startOfToday(),
+  });
+
+  let datesRow: string[] = [];
+  daysInterval.reverse().forEach((day) => {
+    datesRow.push(String(format(day, "dd-eee")));
+  });
+
   return (
     <div className="sticky max-w-[43rem] mx-auto bg-white/60 backdrop-blur-md top-0 h-28 z-20 px-8 md:px-[4.5rem] flex justify-between items-center">
       <p className=" font-medium text-3xl">Habit</p>
       <div className="flex w-[13rem] justify-between">
-        {datesRow.map((date) => (
-          <DateGroup key={date} day={date.split(0, 2)} date={date.split(-2)} />
+        {datesRow.map((item) => (
+          <DateGroup key={item} day={item} date={item} />
         ))}
       </div>
     </div>
