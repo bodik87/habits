1f6231fd54bd5acaa728bf0ced6f29fbe8dcb7a2
Bogdan <bsshul@gmail.com>
2022-12-09 08:28:48 +0200

    +

--- src/components/ui/CardLayout.tsx
+++ src/components/ui/CardLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
+import { IHabitProps } from "../../types";
 
 interface CardLayoutProps {
   title: string;
@@ -10,7 +11,7 @@ interface CardLayoutProps {
   rightLinkText?: string;
   rightLinkPath: string;
   rightBtnText?: string;
-  rightBtnFunction?: () => {};
+  rightBtnFunction?: () => void;
 }
 
 export const CardLayout = ({
@@ -29,15 +30,17 @@ export const CardLayout = ({
       <div
         className={`sticky max-w-[43rem] mx-auto ${habitColor} backdrop-blur-md top-0 h-20 z-20 px-8  flex justify-between items-center border-b border-black/10`}
       >
-        <Link className="font-semibold" to={leftLinkPath}>
+        <Link className="font-semibold min-w-[50px]" to={leftLinkPath}>
           {leftLinkText}
         </Link>
         <div className="text-xl font-semibold">{title}</div>
-        {rightLinkText && (
-          <Link className="font-semibold" to={rightLinkPath}>
-            {rightLinkText}
-          </Link>
-        )}
+        <Link
+          className="font-semibold min-w-[50px]"
+          to={rightLinkPath}
+          onClick={rightBtnFunction}
+        >
+          {rightLinkText}
+        </Link>
       </div>
       <div className="max-w-[43rem] mx-auto">{children}</div>
     </>
--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -6,41 +6,42 @@ import { useTypedSelector } from "../components/hooks/useTypedSelector";
 
 export const Create = () => {
   const [habitColor, setHabitColor] = useState(createConstants.defaultColor);
-  const [titleValue, setTitleValue] = useState('')
+  const [titleValue, setTitleValue] = useState("");
   const { habits } = useTypedSelector((state) => state.habits);
   const { addHabit } = useActions();
 
-  const newHabit = () => {
-    if (titleValue.trim().length) {
-      return {
-        id: habits.length + 1,
+  const createNewHabit = (): any => {
+    return {
+      id: habits.length + 1,
       title: titleValue,
       color: habitColor,
       repeat: 10,
       checkedDays: [],
-      }
-    }
-  }
+    };
+  };
+
+  const newHabit = createNewHabit();
 
   const onClick = () => {
-    
-  }
+    if (titleValue.trim().length) addHabit(newHabit);
+  };
 
   return (
     <CardLayout
-      rightLinkText={createConstants.rightBtn}
+      rightLinkText={titleValue.trim().length ? createConstants.rightBtn : ""}
       leftLinkText={createConstants.leftBtn}
       leftLinkPath="/"
-      rightLinkPath=""
+      rightLinkPath="/"
       title={createConstants.title}
       habitColor={`bg-${habitColor}`}
+      rightBtnFunction={onClick}
     >
       <div className={`flex w-full flex-col bg-${habitColor} px-8 pt-8 pb-4`}>
         <span className="uppercase text-sm">{createConstants.habitName}</span>
         <input
           className="bg-transparent placeholder:text-black/30 text-xl outline-none mt-2"
           value={titleValue}
-          onChange={(e => setTitleValue(e.target.value))}
+          onChange={(e) => setTitleValue(e.target.value)}
           placeholder={createConstants.inputNamePlaceholder}
         />
       </div>
