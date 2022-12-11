7fed16bef725ca9009c0a4a5827ff35b01c0cac7
Bogdan <bsshul@gmail.com>
2022-12-11 23:40:58 +0200

    +

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,7 @@ module.exports = {
         myBlue: "#53D0F0",
         myWhite: "#F8F8F8",
         myDark: "#050505",
-        darkPrimary: "#1D2125",
+        darkPrimary: "#242424",
         darkSecond: "#afb4b8",
       },
       zIndex: {
