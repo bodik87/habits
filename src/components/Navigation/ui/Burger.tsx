e80226e28e4ff3c148bca795d599257daab4a7a3
Bogdan <bsshul@gmail.com>
2022-12-11 15:16:58 +0200

    +

--- src/components/Navigation/ui/Burger.tsx
+++ src/components/Navigation/ui/Burger.tsx
@@ -2,7 +2,7 @@ export const Burger = () => {
   const spanStyle = "bg-myDark h-[2px] w-[2rem]";
 
   return (
-    <div className="w-[2rem] h-[2rem] flex flex-col gap-2 items-center justify-center cursor-pointer">
+    <div className="w-[2rem] h-[2rem] flex flex-col gap-2 items-center justify-center cursor-pointer active:scale-90">
       <span className={spanStyle}></span>
       <span className={spanStyle}></span>
     </div>
